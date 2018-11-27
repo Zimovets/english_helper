375d79123d44dbef33497274749a727eceb521c8
Zimovets Sergey <zimovets_s@ukr.net>
2018-11-27 17:06:42 +0200

    - add bootstap librery

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow () {
 
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index2.html')
+  mainWindow.loadFile('./mdbootstrap/index.html');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
