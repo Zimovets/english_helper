de3b1137b4faa33ced064269cb5135551be8bddd
Zimovets Sergey <zimovets_s@ukr.net>
2018-11-29 18:22:50 +0200

    - add block_test.js - change db.js - change index html

--- static/js/db.js
+++ static/js/db.js
@@ -2,7 +2,9 @@ var Datastore = require('nedb');
 
 var db = new Datastore({filename : 'users', autoload:true});
 
-db.remove({},{multi: true});
+// db.insert({a:5, b:6});
+//
+// db.remove({},{multi: true});
 
 db.find({}, (err, data) => {
     if(err) console.log(err.message);
